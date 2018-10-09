b7b2bac5597cd019ab3fc5f2042227446cac1d0e
Eric Mailman <emailman@dtcc.edu>
2018-10-09 07:59:41 -0400

    Add project files.

---
+++ File Writing Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Writing_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
