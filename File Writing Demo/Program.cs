a3ac378c0a04cc9af76d7090b50147b40b9217e2
Eric Mailman <emailman@dtcc.edu>
2018-10-09 11:50:40 -0400

    First version

--- File Writing Demo/Program.cs
+++ File Writing Demo/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace File_Writing_Demo
 {
@@ -10,6 +8,29 @@ namespace File_Writing_Demo
     {
         static void Main(string[] args)
         {
+            // Create a list of accounts
+            var accounts = new List<Account3>
+            {
+                new Account3("Dopey", 500),
+                new Account3("Sleepy"),
+                new Account3("Sneezy", 300)
+            };
+
+            // Write these records to a file
+            WriteFile(accounts);                
+        }
+
+        static void WriteFile(List<Account3> accts)
+        {
+            StreamWriter outputFile = File.CreateText("accounts.txt");
+            string record;
+            foreach (var acct in accts)
+            {
+                record = $"{acct.Name},{acct.Balance}";
+                Console.WriteLine($"Writing record: {record}");
+                outputFile.WriteLine(record);
+            }
+            outputFile.Close();
         }
     }
 }
